9057e3c2afc9701262a43d08735f388471e2f3a4
m44.soroush <m44.soroush@yahoo.com>
2018-02-14 19:32:28 +0330

    fix mongodb config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose=require("mongoose");
 const dbConfig=require('./db.config');
 
 mongoose.Promise=global.Promise;
-// mongoose.connect(dbConfig.mongoURI);
-mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect(dbConfig.mongoURI);
+// mongoose.connect('mongodb://localhost:27017/Library');
 module.exports=mongoose;
