0d65040709a694b0aec45f029c9fd46b1577c34b
m44.soroush <m44.soroush@yahoo.com>
2018-02-14 18:38:45 +0330

    add mlab to project

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
+const dbConfig=require('./db.config');
 
 mongoose.Promise=global.Promise;
-mongoose.connect("mongodb://localhost:27017/Library");
+mongoose.connect(dbConfig.mongoURI);
 
 module.exports=mongoose;
